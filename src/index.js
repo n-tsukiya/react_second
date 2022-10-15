7da16c634c7c77f42b0f84278cb6515d4681ba86
n-tsukiya <tkmaru555@gmail.com>
2022-10-15 17:14:39 +0900

    セクション6-42

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
